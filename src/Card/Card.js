0080ab95ee522dc50e991d6109f6851c29f8c501
Emily Dittmer <emilyedittmer@gmail.com>
2019-04-13 18:48:15 -0600

    Card information appends and shows if correct or incorrect

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
 import './Card.css';
 
-
 export default class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      randomCard: []
+      randomCards: [],
+      activeGame: false,
+      shownCard: {},
+      incorrectCards: []
     }
   }
 
 startGame = () => {
-  let randomizeCard = this.props.allCards.sort(() => .5 - Math.random());
-  this.setState = {
-    randomCard: this.state.randomCard.push(randomizeCard)
-  }
-console.log(this.state.randomCard);
+  let randomizeCards = this.props.allCards.sort(() => .5 - Math.random());
+  this.setState(
+    {randomCards: randomizeCards,
+    activeGame: true,
+    shownCard: randomizeCards.pop(),
+    }
+  )
 }
-///On click, card renders with prompt and possible answers in button
-  render(props) {
 
+checkAnswer = (event) => {
+  if(event.target.value === this.state.shownCard.correctAnswer) {
+    this.props.increaseScore();
+  } else {
+    this.setState = {
+      incorrectCards: this.state.incorrectCards.push(this.state.shownCard)
+    }
+    console.log(this.state.incorrectCards)
+  }
+}
+
+
+  render() {
+    let toggleStart;
+    if(this.state.activeGame === false) {
+      toggleStart = <input type="button" className="startBtn" onClick={this.startGame} value="Start" />
+    } else {
+      this.state.randomCards.pop()
+      toggleStart =
+        <div>
+          <h3>{this.state.shownCard.prompt}</h3>
+          <input type="button" className="cardBtns" value={this.state.shownCard.possibleChoices[0]} onClick={this.checkAnswer}/>
+          <input type="button" className="cardBtns" value={this.state.shownCard.possibleChoices[1]} onClick={this.checkAnswer}/>
+          <input type="button" className="cardBtns" value={this.state.shownCard.possibleChoices[2]} onClick={this.checkAnswer}/>
+        </div> 
+    }
     return (
       <section className="eachCard">
-        <input type="button" className="startBtn" onClick={this.startGame} value="Start" />
+        {toggleStart}
       </section>
     );
   }
